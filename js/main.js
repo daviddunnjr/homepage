67366169794d37d82ed50e12693c784bb82630a9
David Dunn <98627534+daviddunnjr@users.noreply.github.com>
2025-04-05 23:32:52 -0500

    added datetime

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+////////////////////////
+// Background changer //
+////////////////////////
+
 // Array of image file names in the folder
 const images = ["vapor11_1920x1200.png", "vapor100_1920x1200.png", "vapor103_1920x1200.png", "vapor104_1920x1200.png", "vaporlake_1920x1200.png", "vaporlock_1920x1200.png"];
 
@@ -9,3 +13,25 @@ document.documentElement.style.setProperty(
   "background-image",
   `url('./images/bg/${randomImage}')`
 );
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+// Clock                                                                                             //
+// Src: https://stackoverflow.com/questions/18536726/javascript-to-display-the-current-date-and-time //
+// With edits from me adding updated time every second and symbols to mark date and time.            //
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function clock() {
+  // get a new date (locale machine date time)
+  var date = new Date();
+  // get the date as a string
+  var n = date.toDateString();
+  // get the time as a string
+  var time = date.toLocaleTimeString();
+  // find the html element with the id of datetime
+  // set the innerHTML of that element to the date a space the time
+  document.getElementById('datetime').innerHTML = '&#128467; ' + n + '<br />' + '&#9202; ' + time;
+}
+
+// Update the clock every second.
+// (Does this affect computer resources?)
+window.setInterval(clock, 1000);
